adcb46bbab78da0d0d2b4b3fce946cf2d6db2b06
Wedson Almeida Filho <wedsonaf@google.com>
2021-12-28 23:08:17 +0000

    block/rnbd-clt-sysfs: constify instances of kobj_type
    Mark the kobj_type instances defined in block/rnbd-clt-sysfs code as const.
    Since commit ee6d3dd4ed48 ("driver core: make kobj_type constant."), it is
    possible to define and use constant instances of kobj_type. It is also
    preferable to have them as consts because it reduces the attack surface
    (reduces the amount of writable memory containing function pointers).
    
    Cc: "Md. Haris Iqbal" <haris.iqbal@ionos.com>
    Cc: Jack Wang <jinpu.wang@ionos.com>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: linux-block@vger.kernel.org
    Signed-off-by: Wedson Almeida Filho <wedsonaf@google.com>

--- drivers/block/rnbd/rnbd-clt-sysfs.c
+++ drivers/block/rnbd/rnbd-clt-sysfs.c
@@ -472,7 +472,7 @@ void rnbd_clt_remove_dev_symlink(struct rnbd_clt_dev *dev)
 	}
 }
 
-static struct kobj_type rnbd_dev_ktype = {
+static const struct kobj_type rnbd_dev_ktype = {
 	.sysfs_ops      = &kobj_sysfs_ops,
 	.default_attrs  = rnbd_dev_attrs,
 };
