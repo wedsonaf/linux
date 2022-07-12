1cf4e7bf6623e0ddf907a1a6eb2668819654b9bf
Wedson Almeida Filho <wedsonaf@google.com>
2022-07-12 15:01:40 +0000

    Step 8: add device state

--- samples/rust/rust_scull.rs
+++ samples/rust/rust_scull.rs
@@ -2,6 +2,7 @@
 
 use kernel::io_buffer::{IoBufferReader, IoBufferWriter};
 use kernel::prelude::*;
+use kernel::sync::Ref;
 use kernel::{file, miscdev};
 
 module! {
@@ -10,14 +11,20 @@ module! {
     license: b"GPL",
 }
 
+struct Device {
+    number: usize,
+}
+
 struct Scull {
     _dev: Pin<Box<miscdev::Registration<Scull>>>,
 }
 
 #[vtable]
 impl file::Operations for Scull {
-    fn open(_context: &(), _file: &file::File) -> Result {
-        pr_info!("File was opened\n");
+    type OpenData = Ref<Device>;
+
+    fn open(context: &Ref<Device>, _file: &file::File) -> Result {
+        pr_info!("File for device {} was opened\n", context.number);
         Ok(())
     }
 
@@ -45,7 +52,8 @@ impl file::Operations for Scull {
 impl kernel::Module for Scull {
     fn init(_name: &'static CStr, _module: &'static ThisModule) -> Result<Self> {
         pr_info!("Hello world!\n");
-        let reg = miscdev::Registration::new_pinned(fmt!("scull"), ())?;
+        let dev = Ref::try_new(Device { number: 0 })?;
+        let reg = miscdev::Registration::new_pinned(fmt!("scull"), dev)?;
         Ok(Self { _dev: reg })
     }
 }
