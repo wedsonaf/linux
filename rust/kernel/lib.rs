517d41b7ec01ad459437ebefbaf19dd5ad34c877
Wedson Almeida Filho <walmeida@microsoft.com>
2023-04-10 02:18:54 -0300

    rust: introduce `InPlaceModule`
    This allows modules to be initialised in-place in pinned memory, which
    enables the usage of pinned types (e.g., mutexes, spinlocks, driver
    registrations, etc.) in modules without any extra allocations.
    
    Drivers that don't need this may continue to implement `Module` without
    any changes.
    
    Signed-off-by: Wedson Almeida Filho <walmeida@microsoft.com>

--- rust/kernel/lib.rs
+++ rust/kernel/lib.rs
@@ -21,6 +21,7 @@
 #![feature(new_uninit)]
 #![feature(pin_macro)]
 #![feature(receiver_trait)]
+#![feature(type_alias_impl_trait)]
 #![feature(unsize)]
 
 // Ensure conditional compilation based on the kernel configuration works;
@@ -59,7 +60,7 @@ const __LOG_PREFIX: &[u8] = b"rust_kernel\0";
 /// The top level entrypoint to implementing a kernel module.
 ///
 /// For any teardown or cleanup operations, your type may implement [`Drop`].
-pub trait Module: Sized + Sync {
+pub trait Module: Sized + Sync + Send {
     /// Called at module initialization time.
     ///
     /// Use this method to perform whatever setup or registration your module
@@ -67,6 +68,34 @@ pub trait Module: Sized + Sync {
     ///
     /// Equivalent to the `module_init` macro in the C API.
     fn init(module: &'static ThisModule) -> error::Result<Self>;
+}
+
+/// A module that is pinned and initialised in-place.
+pub trait InPlaceModule: Sync + Send {
+    /// The type that implements the pinned initialisation of the module.
+    type Init: init::PinInit<Self, error::Error>;
+
+    /// Creates an initialiser for the module.
+    ///
+    /// It is called when the module is loaded.
+    fn init(module: &'static ThisModule) -> error::Result<Self::Init>;
+}
+
+impl<T: Module> InPlaceModule for T {
+    type Init = impl init::PinInit<Self, error::Error>;
+
+    fn init(module: &'static ThisModule) -> error::Result<Self::Init> {
+        let m = <Self as Module>::init(module)?;
+
+        let initer = move |slot: *mut Self| {
+            // SAFETY: `slot` is valid for write per the contract with `pin_init_from_closure`.
+            unsafe { slot.write(m) };
+            Ok(())
+        };
+
+        // SAFETY: On success, `initer` always fully initialises an instance of `Self`.
+        Ok(unsafe { init::pin_init_from_closure(initer) })
+    }
 }
 
 /// Equivalent to `THIS_MODULE` in the C API.
