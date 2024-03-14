f67e9dd43eac9eb76f2a220b47094109a2cc1b0d
Wedson Almeida Filho <walmeida@microsoft.com>
2024-03-13 23:52:34 -0300

    samples: rust: remove `Vec` usage from minimal sample
    Remove the only usage of `Vec` in the samples, which gets us closer to
    being able to switch to the upstream `alloc` crate.
    
    Signed-off-by: Wedson Almeida Filho <walmeida@microsoft.com>

--- samples/rust/rust_minimal.rs
+++ samples/rust/rust_minimal.rs
@@ -12,27 +12,18 @@ module! {
     license: "GPL",
 }
 
-struct RustMinimal {
-    numbers: Vec<i32>,
-}
+struct RustMinimal;
 
 impl kernel::Module for RustMinimal {
     fn init(_module: &'static ThisModule) -> Result<Self> {
         pr_info!("Rust minimal sample (init)\n");
         pr_info!("Am I built-in? {}\n", !cfg!(MODULE));
-
-        let mut numbers = Vec::new();
-        numbers.try_push(72)?;
-        numbers.try_push(108)?;
-        numbers.try_push(200)?;
-
-        Ok(RustMinimal { numbers })
+        Ok(RustMinimal)
     }
 }
 
 impl Drop for RustMinimal {
     fn drop(&mut self) {
-        pr_info!("My numbers are {:?}\n", self.numbers);
         pr_info!("Rust minimal sample (exit)\n");
     }
 }
